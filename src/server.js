286af31830a1742a33bff0e6cfffca57c056ba91
Sangwon <jackoss@naver.com>
2020-08-30 11:24:20 +0900

    #3.4 sendMail Function with Nodemailer

--- src/server.js
+++ src/server.js
@@ -1,10 +1,15 @@
-require("dotenv").config();
+import dotenv from "dotenv";
+import path from "path";
+// console.log(__dirname);
+dotenv.config({path: path.resolve(__dirname, ".env")});
 import { GraphQLServer } from "graphql-yoga";
 import logger from "morgan";
 import schema from "~/schema";
+import {sendSecretMail} from "./utils";
+
+// sendSecretMail("jackoss@naver.com", "123")  //nodemailer인증이 안되니 mailgun으로 대체할 것
 
 const PORT = process.env.PORT || 4000;
-
 
 
 //GraphQLServer에 Express가 내장되어 있음.
